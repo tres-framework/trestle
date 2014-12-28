7571dc555938818748ba3c93fb6eb3fa8fd4177f
Julian <firedartonline@gmail.com>
2014-12-28 18:36:59 -0500

    Moved from try/catch to set_exception_handler

--- tests/placeholders.php
+++ tests/placeholders.php
@@ -2,33 +2,31 @@
 
 require_once('inc/autoload.php');
 
+set_exception_handler(function($e) {
+    echo '<b>' . get_class($e) . ':</b> ' . $e->getMessage();
+});
+
 $dbInfo = include('config.php');
 
-try {
-    Trestle\Config::set($dbInfo);
-    
-    $db = new Trestle\Database('MySQL1');
-    
-    $sql = "SELECT `title`
-            FROM `posts`
-            WHERE `author` = :id
-            AND `title` LIKE :like
-            AND `date` > :date
-    ";
-    $args = [
-        ':id' => 1, 
-        ':like' => 'Post%', 
-        ':date' => '2014-11-23'
-    ];
-    
-    $users = $db->query($sql, $args)
-                ->exec();
-    echo '<pre>'; print_r($users->debug()['query']); echo '</pre>';
-    echo '<pre>'; print_r($users->results()); echo '</pre>';
-    echo '<pre>'; print_r($users->debug()); echo '</pre>';
-    echo '<hr />';
-} catch(TrestleException $e) {
-    echo $e->getMessage();
-} catch(Exception $e) {
-    echo $e->getMessage();
-}
+Trestle\Config::set($dbInfo);
+
+$db = new Trestle\Database('MySQL1');
+
+$sql = "SELECT `title`
+        FROM `posts`
+        WHERE `author` = :id
+        AND `title` LIKE :like
+        AND `date` > :date
+";
+$args = [
+    ':id' => 1, 
+    ':like' => 'Post%', 
+    ':date' => '2014-11-23'
+];
+
+$users = $db->query($sql, $args)
+            ->exec();
+echo '<pre>'; print_r($users->debug()['query']); echo '</pre>';
+echo '<pre>'; print_r($users->results()); echo '</pre>';
+echo '<pre>'; print_r($users->debug()); echo '</pre>';
+echo '<hr />';
